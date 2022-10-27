e43738043335c4443a0864c022fcce0b3e60cc63
Julian K <7807570+ColoredCarrot@users.noreply.github.com>
2022-10-27 20:10:22 +0200

    Universally set JVM target to 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,17 @@
 plugins {
+    java
     kotlin("jvm") version "1.7.20"
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+subprojects {
+    plugins.apply("java")
+    java {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
 }
