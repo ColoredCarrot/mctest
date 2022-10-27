325e96429717e78c7f76ad83649acf2978003721
Julian K <7807570+ColoredCarrot@users.noreply.github.com>
2022-10-27 20:13:38 +0200

    Support Spigot 1.18

--- runtime/src/main/kotlin/info/voidev/mctest/runtime/classloading/BukkitBootstrapFixer.kt
+++ runtime/src/main/kotlin/info/voidev/mctest/runtime/classloading/BukkitBootstrapFixer.kt
@@ -7,8 +7,6 @@ import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes
 import org.objectweb.asm.Type
 import org.objectweb.asm.util.CheckClassAdapter
-import org.objectweb.asm.util.TraceClassVisitor
-import java.io.PrintWriter
 
 /**
  * Instruments the bytecode of `org.bukkit.craftbukkit.bootstrap.Main#run`
@@ -70,10 +68,30 @@ private class BukkitBootstrapRunMethodFixer(mv: MethodVisitor?) : MethodVisitor(
         descriptor: String,
         isInterface: Boolean,
     ) {
-        if (name == "<init>" && owner == "java/net/URLClassLoader" && descriptor == "([Ljava/net/URL;)V") {
-            // Load parent classloader onto stack
+        if (name == "<init>" && owner == "java/net/URLClassLoader") {
+            // 1.18.2 uses the constructor with just the URL array argument;
+            // 1.18 passes the URLs as well as the current class' class loader's parent.
+            val whichCtor = when (descriptor) {
+                "([Ljava/net/URL;)V" -> URLClassLoaderConstructor.URL_ARRAY
+                "([Ljava/net/URL;Ljava/lang/ClassLoader;)V" -> URLClassLoaderConstructor.URL_ARRAY_AND_CLASSLOADER
+                else -> throw IllegalStateException("Call to unsupported URLClassLoader constructor")
+            }
+
+            if (whichCtor == URLClassLoaderConstructor.URL_ARRAY_AND_CLASSLOADER) {
+                // We just loaded our class loader's parent class loader onto the stack;
+                // drop that one as we will replace it with our own
+                super.visitInsn(Opcodes.POP)
+            }
+
+            // Load current class loader onto stack (i.e. our bootstrap class loader)
             super.visitVarInsn(Opcodes.ALOAD, 0)
-            super.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/Object", "getClass", "()Ljava/lang/Class;", false)
+            super.visitMethodInsn(
+                Opcodes.INVOKEVIRTUAL,
+                "java/lang/Object",
+                "getClass",
+                "()Ljava/lang/Class;",
+                false
+            )
             super.visitMethodInsn(
                 Opcodes.INVOKEVIRTUAL,
                 "java/lang/Class",
@@ -94,6 +112,11 @@ private class BukkitBootstrapRunMethodFixer(mv: MethodVisitor?) : MethodVisitor(
         }
 
         super.visitMethodInsn(opcode, owner, name, descriptor, isInterface)
+    }
+
+    private enum class URLClassLoaderConstructor {
+        URL_ARRAY,
+        URL_ARRAY_AND_CLASSLOADER,
     }
 }
 
