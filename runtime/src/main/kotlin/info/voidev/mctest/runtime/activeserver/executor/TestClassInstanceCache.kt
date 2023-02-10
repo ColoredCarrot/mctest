24e980bd174ab77bbe0aebde94021a5e046af4a6
Julian K <7807570+ColoredCarrot@users.noreply.github.com>
2023-02-10 14:59:28 +0100

    Fix a deprecation

--- runtime/src/main/kotlin/info/voidev/mctest/runtime/activeserver/executor/TestClassInstanceCache.kt
+++ runtime/src/main/kotlin/info/voidev/mctest/runtime/activeserver/executor/TestClassInstanceCache.kt
@@ -1,13 +1,18 @@
 package info.voidev.mctest.runtime.activeserver.executor
 
 import info.voidev.mctest.runtimesdk.util.IsTestClassWithTests
+import java.lang.reflect.InvocationTargetException
 
 class TestClassInstanceCache : ClassValue<Any>() {
 
     override fun computeValue(type: Class<*>): Any {
         require(IsTestClassWithTests.test(type)) { "Attempted to get test instance for non-test $type" }
 
-        return type.newInstance()
+        try {
+            return type.getConstructor().newInstance()
+        } catch (ex: InvocationTargetException) {
+            throw ex.cause ?: ex
+        }
     }
 
 }
