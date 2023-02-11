e2d8394d31ba2dbf4bd7f2754e26afeec0897096
Julian K <7807570+ColoredCarrot@users.noreply.github.com>
2023-02-11 18:39:20 +0100

    Make TemporaryDirectories thread-safe

--- engine/src/main/kotlin/info/voidev/mctest/engine/util/TemporaryDirectories.kt
+++ engine/src/main/kotlin/info/voidev/mctest/engine/util/TemporaryDirectories.kt
@@ -2,10 +2,11 @@ package info.voidev.mctest.engine.util
 
 import java.nio.file.Files
 import java.nio.file.Path
+import java.util.Collections
 
 object TemporaryDirectories {
 
-    private val pathsToDelete = ArrayList<Path>()
+    private val pathsToDelete = Collections.synchronizedList(ArrayList<Path>())
 
     fun create(prefix: String): Path = Files
         .createTempDirectory(prefix)
